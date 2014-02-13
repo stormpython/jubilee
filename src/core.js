3c2617f204d77129cb0d81ec75871fcbddd1bd92
Shelby Sturgis <shelbyjsturgis@gmail.com>
2014-02-13 15:32:23 -0800

    restructuring the repo

---
+++ src/core.js
@@ -0,0 +1,28 @@
+/**
+ * Created by shelbysturgis on 2/13/14.
+ */
+
+var kbn = window.kbn || {};
+
+kbn.namespace = function (ns_string) {
+  "use strict";
+
+  var parts = ns_string.split('.'),
+      parent = kbn,
+      i;
+
+  // strip redundant leading global
+  if (parts[0] === "kbn") {
+    parts = parts.slice(1);
+  }
+
+  for (i = 0; i < parts.length; i += 1) {
+    // create a property if it doesn't exist
+    if (typeof parent[parts[i]] === "undefined") {
+      parent[parts[i]] = {};
+    }
+    parent = parent[parts[i]];
+  }
+
+  return parent;
+};
