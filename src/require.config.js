9877305dc8de0fa13e44b0f90042293cae769b6d
Shelby Sturgis <shelbyjsturgis@gmail.com>
2015-03-16 11:19:31 -0700

    adding empty test files

--- src/require.config.js
+++ src/require.config.js
@@ -1,8 +1,8 @@
 require.config({
-  baseUrl: "../",
+  baseUrl: "/",
   paths: {
-    elasti: "src/index",
-    d3: "lib/d3/d3"
+    elasti: "/src/index",
+    d3: "/lib/d3/d3"
   },
   shim: {},
   packages: []
