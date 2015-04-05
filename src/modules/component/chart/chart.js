d996ee412f290ef95b8555ab4ba5d45960137137
Shelby Sturgis <shelbyjsturgis@gmail.com>
2015-04-05 12:30:02 -0400

    finsihing chart component

--- src/modules/component/chart/chart.js
+++ src/modules/component/chart/chart.js
@@ -3,6 +3,7 @@ define(function (require) {
 
   return function chart() {
     var gClass = "layer";
+    var chartClass = "chart";
     var transform = null;
     var datum = null;
     var draw = null;
@@ -17,11 +18,42 @@ define(function (require) {
         g.selectAll("g")
           .data(function (d) { return d; })
           .enter().append("g")
+          .attr("class", chartClass)
           .attr("transform", transform)
           .datum(datum)
           .call(draw);
       });
     }
+
+    component.gClass = function (_) {
+      if (!arguments.length) { return gClass; }
+      gClass = _;
+      return component;
+    };
+
+    component.chartClass = function (_) {
+      if (!arguments.length) { return chartClass; }
+      chartClass = _;
+      return component;
+    };
+
+    component.transform = function (_) {
+      if (!arguments.length) { return transform; }
+      transform = _;
+      return component;
+    };
+
+    component.datum = function (_) {
+      if (!arguments.length) { return datum; }
+      datum = _;
+      return component;
+    };
+
+    component.draw = function (_) {
+      if (!arguments.length) { return draw; }
+      draw = _;
+      return component;
+    };
 
     return component;
   };
