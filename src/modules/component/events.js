e68d72dd10ecbdb3ae69e1dbee08ce93e118216a
Shelby Sturgis <shelbyjsturgis@gmail.com>
2015-08-26 11:55:03 -0700

    troubleshooting getting the proper mouse coordinates using binary search

--- src/modules/component/events.js
+++ src/modules/component/events.js
@@ -3,12 +3,39 @@
  */
 define(function (require) {
   var d3 = require("d3");
-  var valuator = require("src/modules/valuator");
   var targetIndex = require("src/modules/helpers/target_index");
+
+  function comparator(target) {
+    var threshold = 100;
+
+    return function (val) {
+      var isWithinThreshold = (val - threshold) < target && (val + threshold) > target;
+      if (isWithinThreshold) {
+        return 0;
+      }
+      return val > threshold ? 1 : -1;
+    };
+  }
+
+  function makeBinarySearch(arr, comparator, accessor) {
+    if (arr.length === 1) { return null; }
+    var midPoint = Math.floor(arr.length / 2);
+    var midValue = accessor(arr[midPoint]);
+
+    if (comparator(midValue) === 1) {
+      return makeBinarySearch(arr.slice(midPoint), comparator, accessor);
+    }
+    if (comparator(midValue) === -1) {
+      return makeBinarySearch(arr.slice(0, midPoint), comparator, accessor);
+    }
+
+    return arr[midPoint];
+  }
 
   return function events() {
     // Private variables
     var listeners = {};
+    var xScale = null;
 
     function component(selection) {
       selection.each(function () {
@@ -29,10 +56,21 @@ define(function (require) {
               var svg = d3.event.target.farthestViewportElement;
               var target = d3.select(d3.event.target);
               var parent = !svg ? d3.select(d3.event.target) : d3.select(svg);
-              var datum = target.datum();
+              //var datum = target.datum();
               var index = targetIndex(parent, target) || 0;
 
-              listener.call(this, d3.event, datum, index);
+              var parentDatum = parent.datum();
+              var coordinates = d3.mouse(parent.select("g").node());
+              var tart = xScale.invert(coordinates[0]).getTime();
+
+              var accessor = function (d) { return d.x; };
+              var comp = comparator(tart);
+              var chaChing = parentDatum.map(function (datum) {
+                return makeBinarySearch(datum, comp, accessor);
+              });
+              console.log(chaChing);
+
+              //listener.call(this, d3.event, datum, index);
             });
           });
         });
@@ -43,6 +81,12 @@ define(function (require) {
     component.listeners = function (_) {
       if (!arguments.length) { return listeners; }
       listeners = _;
+      return component;
+    };
+
+    component.xScale = function (_) {
+      if (!arguments.length) { return xScale; }
+      xScale = _;
       return component;
     };
 
